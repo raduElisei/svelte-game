7dc85a0c4f4aa7cb7d2e82f4a5a8e135c7330b80
Radu Elisei <radu.elisei@yahoo.com>
2023-08-14 18:27:50 +0300

    Add CSS, functions And Emojis

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { emojiList } from "./emoji";
+
+    type State = 'start' | 'playing' | 'paused' | 'won' | 'lost'
+
+    let state: State = 'start'
+    let size = 20
+    let grid = createdGrid()
+    let maxMatches = grid.length / 2
+    let selected: number[] = []
+    let matches: string[] = []
+
+    function createdGrid() {
+        let cards = new Set<string>()
+        let maxSize = size / 2
+
+        while (cards.size < maxSize) {
+            const randomIndex = Math.floor(Math.random() * emojiList.length)
+            cards.add(emojiList[randomIndex])
+        }
+
+        return shuffle([...cards, ...cards])
+    }
+
+    function shuffle<Items>(array: Items[]) {
+        return array.sort(() => Math.random() - 0.5)
+    }
+
+    console.log(grid)
+</script>
+
+Hello
